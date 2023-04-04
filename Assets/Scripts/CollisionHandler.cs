c605254c1c210d3744eeb676ea9f3c2fab1d5566
AndreD4 <andremai629@gmail.com>
2023-04-04 11:51:57 +0900

    onCollision - switch (other.gameObject.tag)

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+  void OnCollisionEnter(Collision other)
+  {
+    switch (other.gameObject.tag)
+    {
+      case "Friendly":
+          Debug.Log("this is friendly");
+          break;
+      case "Finish":
+          Debug.Log("you have landed");
+          break;
+      case "Fuel":
+          Debug.Log("you have fueled up");
+          break;
+
+    }
+  }
+}
