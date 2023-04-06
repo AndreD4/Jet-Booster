b4f640d75654621b1d3d8185acf5c386cc24da7f
AndreD4 <andremai629@gmail.com>
2023-04-06 09:06:04 +0900

    Debug.log you have escaped

--- Assets/Scripts/QuitApp.cs
+++ Assets/Scripts/QuitApp.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class QuitApp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.Escape))
+        {
+          Debug.Log("YOU HAVE ESCAPED");
+        }
     }
 }
